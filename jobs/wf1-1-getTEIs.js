dd3c186c771c4c86463f95ebf49a2170e99f9060
mtuchi <mtuchidev@gmail.com>
2023-05-29 20:01:55 +0300

    create patient record

--- jobs/wf1-1-getTEIs.js
+++ jobs/wf1-1-getTEIs.js
@@ -1,14 +1,14 @@
+// query: 'inactive:eq:true',
+// filter: ['inactive:eq:true'], //option 2 - TBD which option works
+// inactive: true, //option 1 to filter by inactive
 get(
   'trackedEntityInstances',
   {
     ou: 'l22DQq4iV3G',
     program: 'uGHvY5HFoLG',
-    // query: 'inactive:eq:true',
-    // filter: ['inactive:eq:true'], //option 2 - TBD which option works
-    // inactive: true, //option 1 to filter by inactive
   },
   state => {
-    console.log('TEI data ::', JSON.stringify(state.data));
+    // console.log('TEI data ::', JSON.stringify(state.data));
     return state;
   }
 );
--- jobs/wf1-2-createPatients.js
+++ jobs/wf1-2-createPatients.js
@@ -1,38 +1,65 @@
+// TODO: Confirm how these options are coded in openMRS
 fn(state => {
-  const mappings = state.data(d => {
+  const genderOptions = {
+    male: 'eQkRH9GTjBN',
+    female: 'H61UkMHyNku',
+    unknown: 'YJw3CPNftrE',
+    'Transgender female': 'l8RshnosXNy',
+    'Transgender male': 'OcJsz1BOHTf',
+    'Prefer not to answer': 'QQzTV76xGfF',
+    'Gender variant - Non conforming': 'V8RXdmOdY8L',
+  };
+  return { ...state, genderOptions };
+});
+
+fn(state => {
+  const genders = { male: 'M', female: 'F' };
+  const { trackedEntityInstances } = state.data;
+
+  const pluckAttributeValue = (arr, keyVal) => {
+    const result = arr.filter(a => a.code === keyVal);
+    return result.length > 0 ? result[0].value : undefined;
+  };
+
+  const calculateDOB = age => {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const birthYear = currentYear - age;
+
+    const birthday = `${birthYear}-${currentDate.getMonth()}-${currentDate.getDate()}`;
+
+    return birthday;
+  };
+  const patients = trackedEntityInstances.map(d => {
     return {
       identifiers: [
         {
-          identifierType: '',
-          preferred: '',
-          identifier: '',
+          identifierType: '05a29f94-c0ed-11e2-94be-8c13b969e334',
+          preferred: true,
+          identifier: pluckAttributeValue(d.attributes, 'patient_number').match(
+            /\b\d+\b/g
+          )[0],
         },
       ],
       person: {
-        gender: 'M',
-        age: 42,
-        birthdate: '1970-01-01T00:00:00.000+0100',
-        birthdateEstimated: false,
-        dead: false,
-        deathDate: null,
-        causeOfDeath: null,
+        gender: genders[pluckAttributeValue(d.attributes, 'sex')],
+        age: pluckAttributeValue(d.attributes, 'age'),
+        birthdate: calculateDOB(pluckAttributeValue(d.attributes, 'age')),
+        birthdateEstimated: true,
         names: [
           {
             givenName: 'Doe',
             familyName: 'John',
           },
         ],
-        addresses: [
-          {
-            address1: '30, Vivekananda Layout, Munnekolal,Marathahalli',
-            cityVillage: 'Bengaluru',
-            country: 'India',
-            postalCode: '560037',
-          },
-        ],
       },
     };
   });
 
-  return { ...state, mappings };
+  return { ...state, patients };
 });
+
+each(
+  'patients[*]',
+  createPatient(state => state.data)
+);
