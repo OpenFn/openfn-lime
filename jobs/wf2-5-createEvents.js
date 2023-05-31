1e965fa8cdce9d19fe4ac37e71c073dc93734cc6
mtuchi <mtuchidev@gmail.com>
2023-05-31 16:04:26 +0300

    create events

--- jobs/wf2-5-createEvents.js
+++ jobs/wf2-5-createEvents.js
@@ -1,42 +1,45 @@
-each('encounters[*]', state => {
+each('encounters[*]', async state => {
   const { oclMappings, data } = state;
+
+  const encounterDate = data.encounterDatetime.replace('+0000', '');
 
   const pluckObs = arg => data.obs.filter(ob => ob.concept.uuid === arg)[0];
   const pluckOcl = arg =>
-    oclMappings.filter(ocl => ocl.from_concept_name_resolved === arg);
+    oclMappings.filter(ocl => ocl.from_concept_name_resolved === arg)[0];
 
-    
-  if (pluckObs('da33d74e-33b3-495a-9d7c-aa00a-aa0160')) {
-    const encounter = oclMappings.filter(
-      ocl =>
-        ocl.from_concept_name_resolved ===
-        pluckObs('da33d74e-33b3-495a-9d7c-aa00a-aa0160').concept.display
-    );
+  const obs1 = pluckObs('da33d74e-33b3-495a-9d7c-aa00a-aa0160');
+  const obs2 = pluckObs('da33d74e-33b3-495a-9d7c-aa00a-aa0177');
 
-    console.log(
-      pluckObs('da33d74e-33b3-495a-9d7c-aa00a-aa0160').concept.display
-    );
+  const oclMap1 = obs1 && pluckOcl(obs1.concept.display);
+  const oclMap2 = obs2 && pluckOcl(obs2.concept.display);
 
-    // console.log(encounter);
-  }
+  const valueForEncounter1 = oclMap1 && oclMap1.to_concept_name_resolved;
+  const valueForEncounter2 = oclMap2 && oclMap2.to_concept_name_resolved;
+
+  if (typeof valueForEncounter1 === 'undefined')
+    console.error(`Could not find the value for 'ZTSBtZKc8Ff'`);
+
+  if (typeof valueForEncounter2 === 'undefined')
+    console.error(`Could not find the value for 'vqGFXhDM1XG'`);
+
+  (await (valueForEncounter1 && valueForEncounter2)) &&
+    create('events', {
+      program: 'uGHvY5HFoLG',
+      orgUnit: 'l22DQq4iV3G',
+      programStage: 'hfKSeo6nZK0',
+      trackedEntityInstance: data.patient.uuid,
+      eventDate: encounterDate,
+      dataValues: [
+        {
+          dataElement: 'ZTSBtZKc8Ff',
+          value: valueForEncounter1,
+        },
+        {
+          dataElement: 'vqGFXhDM1XG',
+          value: valueForEncounter2,
+        },
+      ],
+    })(state);
 
   return state;
-
-  //   return create('events', {
-  //     program: 'uGHvY5HFoLG',
-  //     orgUnit: 'l22DQq4iV3G',
-  //     programStage: 'hfKSeo6nZK0',
-  //     trackedEntityInstance: encounter.patient.uuid,
-  //     eventDate: '2023-05-30T00:00:00.000',
-  //     dataValues: [
-  //       {
-  //         dataElement: 'ZTSBtZKc8Ff',
-  //         value: 'Acute asthma',
-  //       },
-  //       {
-  //         dataElement: 'vqGFXhDM1XG',
-  //         value: 'Yellow',
-  //       },
-  //     ],
-  //   })(state);
 });
