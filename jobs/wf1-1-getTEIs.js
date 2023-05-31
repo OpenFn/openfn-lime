faa31cef24367e0b2ad2b495e361dcf62e4a3da0
Aleksa Krolls <aleksagk@gmail.com>
2023-05-31 08:26:54 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/wf1-1-getTEIs.js
+++ jobs/wf1-1-getTEIs.js
@@ -1,10 +1,10 @@
-// Get trackedEntityInstances from DHIS2
-// TODO: How to filter based on inactive?
+// Get trackedEntityInstances that are "active" in the target program
 get(
   'trackedEntityInstances',
   {
     ou: 'l22DQq4iV3G',
     program: 'uGHvY5HFoLG',
+    programStatus: 'ACTIVE'
   },
   state => {
     const { trackedEntityInstances } = state;
