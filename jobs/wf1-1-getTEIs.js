60b4a8c69eac93956bd1d20e23601a2f3e1a244f
mtuchi <mtuchidev@gmail.com>
2023-06-01 10:00:39 +0300

    add cursor for get trackedEntityInstances

--- jobs/wf1-1-getTEIs.js
+++ jobs/wf1-1-getTEIs.js
@@ -1,14 +1,32 @@
+fn(state => {
+  const manualCursor = '2023-06-01T08:00:00.000';
+
+  const cursor =
+    state.lastRunDateTime != null && state.lastRunDateTime != ''
+      ? state.lastRunDateTime
+      : manualCursor;
+
+  return { ...state, cursor };
+});
+
 // Get trackedEntityInstances that are "active" in the target program
 get(
   'trackedEntityInstances',
   {
     ou: 'l22DQq4iV3G',
     program: 'uGHvY5HFoLG',
-    programStatus: 'ACTIVE'
+    programStatus: 'ACTIVE',
   },
+  {},
   state => {
-    const { trackedEntityInstances } = state.data;
-    console.log('# of TEIs extracted ::', trackedEntityInstances.length)
-    return { ...state, trackedEntityInstances };
+    const trackedEntityInstances = state.data.trackedEntityInstances.filter(
+      tei => tei.created > state.cursor
+    );
+    const lastRunDateTime = new Date().toISOString();
+
+    console.log('# of TEIs extracted ::', trackedEntityInstances.length);
+
+    console.log('Next sync start date:', lastRunDateTime);
+    return { ...state, trackedEntityInstances, lastRunDateTime };
   }
 );
