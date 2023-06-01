d5e88e4fff14bc4cd95ee44daa55c3d41a2cf281
mtuchi <mtuchidev@gmail.com>
2023-06-01 18:11:11 +0300

    use query option and callback

--- jobs/wf2-4-getOclMap.js
+++ jobs/wf2-4-getOclMap.js
@@ -1,10 +1,21 @@
 // Fetch OCL mappings
 get(
-  'mappings/?q=&page=1&exact_match=off&limit=200&verbose=false&fromConceptOwner=MSFOCG&toConceptOwner=MSFOCG&toConceptSource=DHIS2DataElements&sortDesc=_score'
+  'mappings',
+  {
+    query: {
+      page: 1,
+      exact_match: 'off',
+      limit: 200,
+      verbose: false,
+      fromConceptOwner: 'MSFOCG',
+      toConceptOwner: 'MSFOCG',
+      toConceptSource: 'DHIS2DataElements',
+      sortDesc: '_score',
+    },
+  },
+  state => {
+    // Add state oclMappings
+    const oclMappings = state.data;
+    return { ...state, data: {}, references: [], response: {}, oclMappings };
+  }
 );
-
-// Add state oclMappings
-fn(state => {
-  const oclMappings = state.data;
-  return { ...state, data: {}, references: [], response: {}, oclMappings };
-});
