fb6b7f7fca1aa3a2fca2a0772f6cd1892d7ff1ae
mtuchi <mtuchidev@gmail.com>
2023-06-06 20:34:16 +0300

    update jobs to use ocl adaptor

--- jobs/wf2-4-getOclMap.js
+++ jobs/wf2-4-getOclMap.js
@@ -2,14 +2,15 @@
 get(
   'mappings',
   {
+    owner: 'MSFOCG',
+    repository: 'collections',
+    repositoryId: 'lime-demo',
+    version: 'HEAD',
     query: {
       page: 1,
       exact_match: 'off',
       limit: 200,
       verbose: false,
-      fromConceptOwner: 'MSFOCG',
-      toConceptOwner: 'MSFOCG',
-      toConceptSource: 'DHIS2DataElements',
       sortDesc: '_score',
     },
   },
@@ -19,3 +20,31 @@ get(
     return { ...state, data: {}, references: [], response: {}, oclMappings };
   }
 );
+
+// get(
+//   'MSFOCG/collections/lime-demo/HEAD/mappings',
+//   {
+//     // owner_type: "users",
+//     page: 1,
+//     exact_match: 'off',
+//     limit: 200,
+//     verbose: false,
+//     sortDesc: '_score',
+//   },
+//   state => {
+//     // Add state oclMappings
+//     const oclMappings = state.data;
+//     return { ...state, data: {}, references: [], response: {}, oclMappings };
+//   }
+// );
+
+// getMappings(
+//   'MSFOCG',
+//   'lime-demo',
+//   { page: 1, exact_match: 'off', verbose: false },
+//   state => {
+//     // Add state oclMappings
+//     const oclMappings = state.data;
+//     return { ...state, data: {}, references: [], response: {}, oclMappings };
+//   }
+// );
