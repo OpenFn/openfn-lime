64156906b7fd3600d09b27428bd87d75f7added2
mtuchi <mtuchidev@gmail.com>
2023-06-07 20:37:01 +0300

    use the cursor from upstream job
    Fixes #17

--- jobs/wf2-3-getEncounters.js
+++ jobs/wf2-3-getEncounters.js
@@ -1,23 +1,3 @@
-// here we define the date cursor
-fn(state => {
-  const manualCursor = '2023-06-01T07:50:00.000'; //'2023-06-01T07:50:00.000'
-
-  //Added this to replace cursor logic below for testing
-  const cursor = manualCursor === '' ? state.lastRunDateTime : manualCursor;
-
-  //TODO: Update OLD CURSOR LOGIC... this never is cleared if we "clear state" in job 1
-  // state.lastRunDateTime != null && state.lastRunDateTime != ''
-  //   ? state.lastRunDateTime
-  //   : manualCursor;
-
-  //FOR TESTING: To re-set job to use manual cursor, comment in the line below
-  //const cursor = manualCursor;
-
-  console.log('Date cursor to filter & get only new encounters ::', cursor);
-
-  return { ...state, cursor };
-});
-
 // Fetch encounters from the date of cursor
 //getEncounters({ q: 'Patient', v: 'full' }); //Query patients with name like 'Patient'
 // OpenMRS demo instance does not support querying ALL records (q=all)
@@ -37,8 +17,5 @@ fn(state => {
   );
   console.log('# of new encounters to sync to dhis2 ::', encounters.length);
 
-  const lastRunDateTime = new Date().toISOString();
-  console.log('Next sync start date:', lastRunDateTime);
-
-  return { ...state, data: {}, references: [], lastRunDateTime, encounters };
+  return { ...state, data: {}, references: [], encounters };
 });
