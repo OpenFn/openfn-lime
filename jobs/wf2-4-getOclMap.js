1fed9a4a31c364c983ea110aecc2b1018a8a3687
Aleksa Krolls <aleksagk@gmail.com>
2023-06-08 16:48:42 +0300

    mtuchi@openfn.org's change on OpenFn.org

--- jobs/wf2-4-getOclMap.js
+++ jobs/wf2-4-getOclMap.js
@@ -3,26 +3,34 @@
  **/
 
 // Fetch OCL mappings
-get(
-  'orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings/',
-  {
-    page: 1,
-    exact_match: 'off',
-    limit: 200,
-    verbose: false,
-    sortDesc: '_score',
-    fromConceptOwner: 'MSFOCG',
-    toConceptOwner: 'MSFOCG',
-    toConceptSource:'DHIS2DataElements'
+// get(
+//   'orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings/',
+//   {
+//     page: 1,
+//     exact_match: 'off',
+//     limit: 200,
+//     verbose: false,
+//     sortDesc: '_score',
+//     fromConceptOwner: 'MSFOCG',
+//     toConceptOwner: 'MSFOCG',
+//     toConceptSource:'DHIS2DataElements'
     
-  },
-  state => {
-    // Add state oclMappings
-    const oclMappings = state.data;
-    return { ...state, data: {}, references: [], response: {}, oclMappings };
+//   },
+//   state => {
+//     // Add state oclMappings
+//     const oclMappings = state.data;
+//     return { ...state, data: {}, references: [], response: {}, oclMappings };
+//   }
+// );
+
+get(
+  'https://api.openconceptlab.org/orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings/?page=1&exact_match=off&limit=200&verbose=false&fromConceptOwner=MSFOCG&toConceptOwner=MSFOCG&toConceptSource=DHIS2DataElements&sortDesc=_score',
+  {
+    //query: {foo: 'bar', a: 1},
+    headers: { 'content-type': 'application/json' },
+    //authentication: {username: 'user', password: 'pass'}
   }
 );
-
 /*
  * The following implimentation of the get(),
  * works only with the OCL adaptor
