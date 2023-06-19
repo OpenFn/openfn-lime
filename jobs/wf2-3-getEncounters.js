4b86eac438cfa9e8b95c8d4f39c073207e1c8014
Aleksa Krolls <aleksagk@gmail.com>
2023-06-19 10:08:45 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/wf2-3-getEncounters.js
+++ jobs/wf2-3-getEncounters.js
@@ -1,7 +1,7 @@
 // Fetch encounters from the date of cursor
 //getEncounters({ q: 'Patient', v: 'full' }); //Query patients with name like 'Patient'
 // OpenMRS demo instance does not support querying ALL records (q=all)
-getEncounters({ q: 'Patient', v: 'full' });
+getEncounters({ q: 'Patient', v: 'full', limit: 100 });
 
 // Update cursor and return encounters
 fn(state => {
