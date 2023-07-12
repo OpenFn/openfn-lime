ea080e3cd15a20e8b1a7f95518704d1c4b7b2478
aleksa-krolls <aleksagk@gmail.com>
2023-07-12 23:31:59 +0300

    update OCL query to also get extra attributes; comment updates

--- jobs/wf2-4-getOclMap.js
+++ jobs/wf2-4-getOclMap.js
@@ -1,3 +1,45 @@
+// Fetch OCL mappings using ocl get()
+get(
+  'orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings/',
+  {
+    page: 1,
+    limit: 1000,
+    verbose: false,
+    fromConceptOwner: 'MSFOCG',
+    toConceptOwner: 'MSFOCG',
+    toConceptSource: 'DHIS2DataElements',
+    sortDesc: '_score',
+    lookupToConcept: true,
+    verbose: true,
+  },
+  state => {
+    // Add state oclMappings
+    const oclMappings = state.data;
+    console.log(JSON.stringify(oclMappings, null, 2), 'OCL Mappings');
+    return { ...state, data: {}, references: [], response: {}, oclMappings };
+  }
+);
+// Job versions if using different adaptor functions
+// Fetch mappings using ocl getMappings() function
+// getMappings(
+//   'MSFOCG',
+//   'lime-demo',
+//   {
+//     page: 1,
+//     limit: 1000,
+//     verbose: false,
+//     fromConceptOwner: 'MSFOCG',
+//     toConceptOwner: 'MSFOCG',
+//     toConceptSource: 'DHIS2DataElements',
+//     sortDesc: '_score',
+//   },
+//   state => {
+//     // Add state oclMappings
+//     const oclMappings = state.data;
+//     return { ...state, data: {}, references: [], response: {}, oclMappings };
+//   }
+// );
+
 /*
  * Fetching mappings using http get()
  **/
@@ -14,46 +56,6 @@
 //       toConceptOwner: 'MSFOCG',
 //       toConceptSource: 'DHIS2DataElements',
 //     },
-//   },
-//   state => {
-//     // Add state oclMappings
-//     const oclMappings = state.data;
-//     return { ...state, data: {}, references: [], response: {}, oclMappings };
-//   }
-// );
-
-// Fetch OCL mappings using ocl get()
-get(
-  'orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings/',
-  {
-    page: 1,
-    limit: 1000,
-    verbose: false,
-    fromConceptOwner: 'MSFOCG',
-    toConceptOwner: 'MSFOCG',
-    toConceptSource: 'DHIS2DataElements',
-    sortDesc: '_score',
-  },
-  state => {
-    // Add state oclMappings
-    const oclMappings = state.data;
-    console.log(JSON.stringify(oclMappings, null, 2), 'OCL Mappings');
-    return { ...state, data: {}, references: [], response: {}, oclMappings };
-  }
-);
-
-// Fetch mappings using ocl getMappings() function
-// getMappings(
-//   'MSFOCG',
-//   'lime-demo',
-//   {
-//     page: 1,
-//     limit: 1000,
-//     verbose: false,
-//     fromConceptOwner: 'MSFOCG',
-//     toConceptOwner: 'MSFOCG',
-//     toConceptSource: 'DHIS2DataElements',
-//     sortDesc: '_score',
 //   },
 //   state => {
 //     // Add state oclMappings
