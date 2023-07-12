b3f39619e7e2678779ae837a68389d9927e7c626
aleksa-krolls <aleksagk@gmail.com>
2023-07-12 23:28:00 +0300

    update OCL query to also get extra attributes

--- jobs/wf2-4-getOclMap.js
+++ jobs/wf2-4-getOclMap.js
@@ -1,57 +1,26 @@
-/*
- * This get() implementation is backward compatible with the http adaptor
- **/
-
-// Fetch OCL mappings
-// get(
-//   'orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings/',
-//   {
-//     page: 1,
-//     exact_match: 'off',
-//     limit: 200,
-//     verbose: false,
-//     sortDesc: '_score',
-//     fromConceptOwner: 'MSFOCG',
-//     toConceptOwner: 'MSFOCG',
-//     toConceptSource:'DHIS2DataElements'
-    
-//   },
-//   state => {
-//     // Add state oclMappings
-//     const oclMappings = state.data;
-//     return { ...state, data: {}, references: [], response: {}, oclMappings };
-//   }
-// );
-
-// get(
-//   'https://api.openconceptlab.org/orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings/?page=1&exact_match=off&limit=200&verbose=false&fromConceptOwner=MSFOCG&toConceptOwner=MSFOCG&toConceptSource=DHIS2DataElements&sortDesc=_score',
-//   {
-//     //query: {foo: 'bar', a: 1},
-//     headers: { 'content-type': 'application/json' },
-//     //authentication: {username: 'user', password: 'pass'}
-//   }
-// );
-
-get('https://api.openconceptlab.org/orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings', {
-   query: { 
-     page: 1,
-     limit: 1000,
-     verbose: false, 
-     fromConceptOwner: 'MSFOCG', 
-     toConceptOwner: 'MSFOCG',
-     toConceptSource: 'DHIS2DataElements',
-     sortDesc: '_score'
-   },
-   headers: {'content-type': 'application/json'},
-   //authentication: {username: 'user', password: 'pass'}
- },
- state => {
+get(
+  'https://api.openconceptlab.org/orgs/MSFOCG/collections/lime-demo/HEAD/expansions/autoexpand-HEAD/mappings',
+  {
+    query: {
+      page: 1,
+      limit: 1000,
+      fromConceptOwner: 'MSFOCG',
+      toConceptOwner: 'MSFOCG',
+      toConceptSource: 'DHIS2DataElements',
+      sortDesc: '_score',
+      lookupToConcept: true,
+      verbose: true,
+    },
+    headers: { 'content-type': 'application/json' },
+    //authentication: {username: 'user', password: 'pass'}
+  },
+  state => {
     // Add state oclMappings
     const oclMappings = state.data;
-    console.log(JSON.stringify(oclMappings, null, 2), "OCL Mappings")
+    console.log(JSON.stringify(oclMappings, null, 2), 'OCL Mappings');
     return { ...state, data: {}, references: [], response: {}, oclMappings };
   }
-  ); 
+);
 /*
  * The following implimentation of the get(),
  * works only with the OCL adaptor
@@ -68,6 +37,8 @@ get('https://api.openconceptlab.org/orgs/MSFOCG/collections/lime-demo/HEAD/expan
 //     limit: 200,
 //     verbose: false,
 //     sortDesc: '_score',
+//     lookupToConcept: true,
+//     verbose: true,
 //   },
 //   state => {
 //     // Add state oclMappings
@@ -79,7 +50,7 @@ get('https://api.openconceptlab.org/orgs/MSFOCG/collections/lime-demo/HEAD/expan
 // getMappings(
 //   'MSFOCG',
 //   'lime-demo',
-//   { page: 1, exact_match: 'off', verbose: false },
+//   { page: 1, exact_match: 'off', verbose: true, lookupToConcept: true },
 //   state => {
 //     // Add state oclMappings
 //     const oclMappings = state.data;
